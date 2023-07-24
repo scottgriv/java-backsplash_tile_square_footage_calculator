4fde2ef282e14a3aa8660d0d88e312d91e376ab8
Scott Griv <scottgrivner@gmail.com>
2023-07-24 19:10:11 -0400

    Initial Commit
    Initial Commit

---
+++ BacksplashCalculator.java
@@ -0,0 +1,95 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BacksplashCalculator {
+    public void calculateCasesNeeded() {
+        // Code to calculate the number of cases needed based on user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Instructions
+        System.out.println("\n.:`:. Welcome to the Backsplash Calculator! .:`:.\n\n" +
+                "Let's begin! \n");
+
+        double widthFeet = 0;
+        double widthInches = 0;
+        double lengthFeet = 0;
+        double lengthInches = 0;
+        boolean validInput = false;
+        String errorString = "Error: Invalid input! Let's start again, please enter a valid number.";
+
+        do {
+            try {
+                System.out.println("Enter the width of the surface area in feet:");
+                widthFeet = scanner.nextDouble();
+
+                System.out.println("Enter the width of the surface area in inches:");
+                widthInches = scanner.nextDouble();
+
+                System.out.println("Enter the length of the surface area in feet:");
+                lengthFeet = scanner.nextDouble();
+
+                System.out.println("Enter the length of the surface area in inches:");
+                lengthInches = scanner.nextDouble();
+
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println(errorString + "\n");
+                scanner.nextLine(); // Clear the input buffer to prevent an infinite loop
+            }
+        } while (!validInput);
+
+        // Convert dimensions to total inches
+        double totalWidthInches = (widthFeet * 12) + widthInches;
+        double totalLengthInches = (lengthFeet * 12) + lengthInches;
+
+        // Calculate total area in square inches
+        double totalAreaSquareInches = totalWidthInches * totalLengthInches;
+
+        // Convert total area to square feet
+        double totalAreaSquareFeet = totalAreaSquareInches / 144;
+
+        // Output the calculated square footage of the surface area
+        System.out.println("\nThe total square feet of the surface area is: " + totalAreaSquareFeet + " sq ft.\n");
+
+        double caseWidthInches = 0;
+        double caseLengthInches = 0;
+        int caseQuantity = 0;
+        validInput = false;
+
+        do {
+            try {
+                System.out.println("Enter the width of a single backsplash tile in inches:");
+                caseWidthInches = scanner.nextDouble();
+
+                System.out.println("Enter the length of a single backsplash tile in inches:");
+                caseLengthInches = scanner.nextDouble();
+
+                System.out.println("Enter the backsplash tile case quantity:");
+                caseQuantity = scanner.nextInt();
+
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println(errorString + "\n");
+                scanner.nextLine(); // Clear the input buffer to prevent an infinite loop
+            }
+        } while (!validInput);
+
+        // Calculate case area in square inches
+        double caseAreaSquareInches = caseWidthInches * caseLengthInches;
+
+        // Convert case area to square feet
+        double caseAreaSquareFeet = caseAreaSquareInches / 144;
+
+        // Calculate number of cases needed and round up to the nearest integer
+        int casesNeeded = (int) Math.ceil(totalAreaSquareFeet / caseAreaSquareFeet / caseQuantity);
+
+        System.out.println("\nNumber of backsplash tile cases needed to cover your surface area:\n" + casesNeeded + " Cases\n");
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        BacksplashCalculator calculator = new BacksplashCalculator();
+        calculator.calculateCasesNeeded();
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Backsplash Tile Square Footage Calculator
+A calculator that will assist in determining the number of backsplash tile cases/packs to purchase, based on the square footage using the width and length (in feet and/or inches) of a surface area.
+
+## Background
+I was recently looking to purchase a case of [backsplash floor and wall, peel-and-stick tiles](https://www.homedepot.com/c/localad?auto_locate=true&auto_store=true&flyer_run_id=867085&flyer_type_name=catalog). One problem that I ran into, was that some websites did not have a square footage calculator from inches. I wanted to input width and length in inches of my surface area (wall or floor), and get an output for the number of cases that I needed to purchase to cover the input square footage, so I built this Java program to calculate what I needed. Enjoy!
+
+<div align="center">
+    <img src="./docs/images/Tile_Example.png" width="75%" height="75%" alt="Demo">
+</div>
+
+## Installation Instructions
+1. Make sure you have Java installed.
+2. Download the program files.
+3. Open a command line, and navigate to the directory the program is in.
+4. Compile the program files by calling: ```javac BacksplashCalculator.java```
+5. Run the program by calling: ```java BacksplashCalculator```
+6. Follow the program input prompts using the `Program Instructions` below.
+
+## Program Instructions
+1. Use a measuring tape to take the dimensions (width & length) of a surface area.
+2. Enter the width & length of the surface area in feet and inches (numbers only, no letters). <br>
+**Note:** If you're measuring the surface area only in feet or inches, input 0 when prompted.
+3. Enter the width and length of a single backsplash tile (in inches).
+4. Enter the total number of backsplash tiles that come in a single case.
+5. The program will calculate the total number of cases you need to purchase to cover your surface area. <br>
+**Note:** The suggestion may have extra tiles/square footage because we're rounding up to ensure we have enough tiles to cover the input surface area.
+
+## License
+This repository is released under the **MIT License**. [See LICENSE](LICENSE) for details.
+
+## Credit
+**Author:** [Scott Grivner](https://github.com/scottgriv)<br>
+**Email:** scott.grivner@gmail.net<br>
+**Website:** [nightowllabs.net](https://www.nightowllabs.net)<br>
+**Reference:** [Main Branch](https://github.com/scottgriv/java-backsplash_square_footage_calculator)
